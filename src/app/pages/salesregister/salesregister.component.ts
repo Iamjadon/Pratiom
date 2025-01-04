2f48ec231a1155b82cba97aa4f2d05bf290e5f4d
rajveerkanwer11 <122509525+rajveerkanwer11@users.noreply.github.com>
2025-01-04 14:26:40 +0530

    create login and sales component

--- src/app/pages/salesregister/salesregister.component.ts
+++ src/app/pages/salesregister/salesregister.component.ts
@@ -9,7 +9,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from '@angular/router';
-import { Item } from '../../models/Item.model';
 
 @Component({
   selector: 'app-salesregister',
@@ -24,7 +23,7 @@ import { Item } from '../../models/Item.model';
     MatSelectModule,
     MatButtonModule,
     MatCardModule,
-    MatDatepickerModule, 
+    MatDatepickerModule,
     MatNativeDateModule,
   ],
   templateUrl: './salesregister.component.html',
@@ -32,27 +31,37 @@ import { Item } from '../../models/Item.model';
 })
 export class SalesregisterComponent {
   partyName: string = '';
+  address: string = '';
+  mobile: string = '';
+  phone: string = '';
+  email: string = '';
   invoiceNo: string = '#PR001';
+  gstNo: string = '';
   invoiceDate: Date = new Date();
 
-  newItem: Item = {
+  newItem = {
     type: '',
     name: '',
     qty: 0,
     weight: 0,
     price: 0,
     discount: 0,
+    total: 0,
+    Karat: '',
+    Stone: 0,
+    StoneAmt: 0,
+    NetMetal: 0,
+    totalvalue: 0,
   };
 
-  items: Item[] = [];
+  items: any[] = [];
   totalBill: number = 0;
   discount: number = 0;
   sgst: number = 0;
   cgst: number = 0;
 
   addItem() {
-    const calculatedTotal = (this.newItem.qty * this.newItem.price) - this.newItem.discount;
-    this.items.push({ ...this.newItem, total: calculatedTotal });
+    this.items.push({ ...this.newItem });
 
     this.newItem = {
       type: '',
@@ -61,6 +70,12 @@ export class SalesregisterComponent {
       weight: 0,
       price: 0,
       discount: 0,
+      total: 0,
+      Karat: '',
+      Stone: 0,
+      StoneAmt: 0,
+      NetMetal: 0,
+      totalvalue: 0,
     };
 
     this.calculateTotal();
@@ -72,10 +87,11 @@ export class SalesregisterComponent {
     this.sgst = 0;
     this.cgst = 0;
 
-    this.items.forEach(item => {
-      this.totalBill += item.total || 0;
-      this.sgst += item.total ? item.total * 0.09 : 0;
-      this.cgst += item.total ? item.total * 0.09 : 0;
+    this.items.forEach((item) => {
+      item.total = item.qty * item.price - item.discount;
+      item.sgst = item.total * 1.5;
+      item.cgst = item.total * 1.5;
+      this.totalBill += item.total + item.sgst + item.cgst;
     });
   }
 }
